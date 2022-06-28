30936cf70d46182f25a726921e1434c87bb0aec1
Joao Victor <jovi47profissional@gmail.com>
2022-06-28 15:18:30 -0300

    Final project sample

---
+++ kv_umbrella/mix.exs
@@ -0,0 +1,30 @@
+defmodule KvUmbrella.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      apps_path: "apps",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      releases: [
+        foo: [
+          version: "0.0.1",
+          applications: [kv_server: :permanent, kv: :permanent]
+        ],
+        bar: [
+          version: "0.0.1",
+          applications: [kv: :permanent]
+        ]
+      ]
+    ]
+  end
+
+  # Dependencies listed here are available only for this
+  # project and cannot be accessed from applications inside
+  # the apps folder.
+  #
+  # Run "mix help deps" for examples and options.
+  defp deps do
+    []
+  end
+end
